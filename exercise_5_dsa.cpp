a5db448ebee1aa322dae63ff3c59d47c60d1a4f8
North Nygel G. Potestades <qnngpotestades@tip.edu.ph>
2024-09-13 17:12:45 +0800

    Add files via upload

---
+++ exercise_5_dsa.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+//creating a node
+class Node {
+    public:
+    int value;
+    Node* next;
+};
+
+int main() {
+    Node* head;
+    Node* one = NULL;
+    Node* two = NULL;
+    Node* three = NULL;
+
+    //allocate 3 nodes in the heap
+    one = new Node();
+    two = new Node();
+    three = new Node();
+
+    //assign value values
+    one->value = 1;
+    two->value = 2;
+    three->value = 3;
+
+    //connect nodes
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    //print the linked list value
+    head = one;
+    while (head != NULL) {
+        cout << head->value;
+        head = head->next;
+    }
+
+    return 0;
+}
