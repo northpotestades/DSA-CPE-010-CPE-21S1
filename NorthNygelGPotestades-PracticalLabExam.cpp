20458a62d120a9729876f0a050a7fd2f4d126a5f
North Nygel G. Potestades <qnngpotestades@tip.edu.ph>
2024-09-30 16:50:59 +0800

    Add files via upload

---
+++ NorthNygelGPotestades-PracticalLabExam.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int numbers[] = {5, 4, 3, 2, 1};
+	int length = size(numbers);
+	cout << "The length of the array is: " << length << "." << endl;
+	return 0;
+};
